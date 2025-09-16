ad7f234bf5f8520b3899b0c7fdb297ba79ed103a
AILTON <ailtonfsilva05@gmail.com>
2025-09-16 18:35:36 -0300

    att
    att

--- calculadora/styles.py
+++ calculadora/styles.py
@@ -1,31 +1,61 @@
-# QSS - QT Styles for Python
-# https://doc.qt.io/qtforpython-6/tutorials/basictutorial/widgetstyling.html
-# DarkStyles
-# https://pypi.org/project/QDarkStyle/
+# # QSS - QT Styles for Python
+# # https://doc.qt.io/qtforpython-6/tutorials/basictutorial/widgetstyling.html
+# # DarkStyles
+# # https://pypi.org/project/QDarkStyle/
+
+# import qdarkstyle
+# from variables import (DARKER_PRIMARY_COLOR, DARKEST_PRIMARY_COLOR, PRIMARY_COLOR)
+ 
+# qss = f"""
+#     PushButton[cssClass="specialButton"] {{
+#         color: #fff;
+#         background: {PRIMARY_COLOR};
+#         border-radius: 5px;
+#     }}
+#     PushButton[cssClass="specialButton"]:hover {{
+#         color: #fff;
+#         background: {DARKER_PRIMARY_COLOR};
+#     }}
+#     PushButton[cssClass="specialButton"]:pressed {{
+#         color: #fff;
+#         background: {DARKEST_PRIMARY_COLOR};
+#     }}
+# """
+ 
+ 
+# def setupTheme(app):
+#     # Aplicar o estilo escuro do qdarkstyle
+#     app.setStyleSheet(qdarkstyle.load_stylesheet_pyside6())
+ 
+#     # Sobrepor com o QSS personalizado para estilização adicional
+#     app.setStyleSheet(app.styleSheet() + qss)
 
 import qdarkstyle
 from variables import (DARKER_PRIMARY_COLOR, DARKEST_PRIMARY_COLOR, PRIMARY_COLOR)
- 
+
+# Seu QSS personalizado
 qss = f"""
-    PushButton[cssClass="specialButton"] {{
+    QPushButton[cssClass="specialButton"] {{
         color: #fff;
         background: {PRIMARY_COLOR};
         border-radius: 5px;
     }}
-    PushButton[cssClass="specialButton"]:hover {{
+    QPushButton[cssClass="specialButton"]:hover {{
         color: #fff;
         background: {DARKER_PRIMARY_COLOR};
     }}
-    PushButton[cssClass="specialButton"]:pressed {{
+    QPushButton[cssClass="specialButton"]:pressed {{
         color: #fff;
         background: {DARKEST_PRIMARY_COLOR};
     }}
 """
- 
- 
+
 def setupTheme(app):
-    # Aplicar o estilo escuro do qdarkstyle
-    app.setStyleSheet(qdarkstyle.load_stylesheet_pyside6())
- 
-    # Sobrepor com o QSS personalizado para estilização adicional
-    app.setStyleSheet(app.styleSheet() + qss)
+    # Carrega o estilo do qdarkstyle
+    qdark_stylesheet = qdarkstyle.load_stylesheet_pyside6()
+    
+    # Combina o estilo do qdarkstyle com o seu estilo personalizado
+    combined_stylesheet = qdark_stylesheet + qss
+    
+    # Aplica o estilo combinado de uma vez só
+    app.setStyleSheet(combined_stylesheet)
